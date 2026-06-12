ea5e426ebe679007fd77f54256d35757341c400f
Sweit2326 <filamilx@gmail.com>
2026-06-12 20:04:30 +0300

    dao + interface -> fully refactored dao

--- src/main/java/com/roadmap/fourth/dao/PlayerDao.java
+++ src/main/java/com/roadmap/fourth/dao/PlayerDao.java
@@ -3,8 +3,30 @@ package com.roadmap.fourth.dao;
 import com.roadmap.fourth.model.Player;
 import org.hibernate.Session;
 
-public interface PlayerDao {
-    Player getPlayerById(int id);
-    Player getPlayerByName(String name);
-    Player postPlayer(String name);
+import static com.roadmap.fourth.util.HibernateUtil.getSessionFactory;
+
+public class PlayerDao {
+//    public Player getPlayerById(int id) {
+//        Session session = getSessionFactory().openSession();
+//        Transaction tx = session.beginTransaction();
+//
+//        Player player = session.find(Player.class, id);
+//        tx.commit();
+//        session.close();
+//        return player;
+//    }
+
+    public Player getPlayerByName(String name) {
+        Session session = getSessionFactory().getCurrentSession();
+
+        return session.createQuery(
+                        "from Player p where p.name = :name", Player.class)
+                .setParameter("name", name)
+                .uniqueResult();
+    }
+
+    public void postPlayer(String name) {
+        Session session = getSessionFactory().getCurrentSession();
+        session.persist(new Player(name));
+    }
 }
