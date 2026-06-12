53187a4ffc3b5a24fa5887c53685931b2b700f79
Sweit2326 <filamilx@gmail.com>
2026-06-12 20:04:48 +0300

    removed @Setter annotation

--- src/main/java/com/roadmap/fourth/model/Player.java
+++ src/main/java/com/roadmap/fourth/model/Player.java
@@ -2,7 +2,6 @@ package com.roadmap.fourth.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
@@ -21,8 +20,14 @@ public class Player {
     private int id;
 
     @Getter
-    @Setter
     @Column(name = "Name", unique = true, nullable = false, length = 50)
     private String name;
+
+    public Player(String name) {
+        if (name == null && name.isBlank()) {
+            throw new IllegalArgumentException("Player name cannot be empty");
+        }
+        this.name = name;
+    }
 }
 
