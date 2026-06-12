d8837f912e9b45929de3c7910d8c979ad427c297
Sweit2326 <filamilx@gmail.com>
2026-06-12 20:04:23 +0300

    dao + interface -> fully refactored dao

--- src/main/java/com/roadmap/fourth/dao/MatchDao.java
+++ src/main/java/com/roadmap/fourth/dao/MatchDao.java
@@ -1,12 +1,35 @@
 package com.roadmap.fourth.dao;
 
+import com.roadmap.fourth.dto.MatchScoreDTO;
 import com.roadmap.fourth.model.Match;
 import com.roadmap.fourth.model.Player;
-
+import org.hibernate.Session;
 import java.util.List;
 
-public interface MatchDao {
-    Match getMatchById(int id);
-    List<Match> getMatchesByPlayerName(String name);
-    Match postMatch(Player stPlayer, Player ndPlayer, Player winner);
+import static com.roadmap.fourth.util.HibernateUtil.getSessionFactory;
+
+public class MatchDao {
+    public Match getMatchById(int id) {
+        Session session = getSessionFactory().getCurrentSession();
+        return session.find(Match.class, id);
+    }
+    public List<Match> getMatchesByPlayerName(String name) {
+        Session session = getSessionFactory().getCurrentSession();
+
+        List<Match> matches = session.createQuery("FROM Match WHERE player1.name = :name OR player2.name = :name ORDER BY id", Match.class)
+                .setParameter("name", name)
+                .getResultList();
+        return matches;
+    }
+    public void postMatch(MatchScoreDTO matchDTO) {
+        PlayerDao plDao = new PlayerDao();
+        Session session = getSessionFactory().getCurrentSession();
+
+        Player stPlayer = plDao.getPlayerByName(matchDTO.getPlayerName(0));
+        Player ndPlayer = plDao.getPlayerByName(matchDTO.getPlayerName(1));
+        Player winner = (stPlayer.getName().equals(matchDTO.getWinner())) ? stPlayer : ndPlayer;
+
+        Match match = new Match(stPlayer, ndPlayer, winner);
+        session.persist(match);
+    }
 }
