4fabeb5f792d75cd59ae3bb05ce0cb3d3ee17309
Sweit2326 <filamilx@gmail.com>
2026-06-17 19:15:20 +0300

    clean code from deprecated method

--- src/main/java/com/roadmap/fourth/dao/PlayerDao.java
+++ src/main/java/com/roadmap/fourth/dao/PlayerDao.java
@@ -6,21 +6,11 @@ import org.hibernate.Session;
 import static com.roadmap.fourth.util.HibernateUtil.getSessionFactory;
 
 public class PlayerDao {
-//    public Player getPlayerById(int id) {
-//        Session session = getSessionFactory().openSession();
-//        Transaction tx = session.beginTransaction();
-//
-//        Player player = session.find(Player.class, id);
-//        tx.commit();
-//        session.close();
-//        return player;
-//    }
-
     public Player getPlayerByName(String name) {
         Session session = getSessionFactory().getCurrentSession();
 
         return session.createQuery(
-                        "from Player p where p.name = :name", Player.class)
+                        "FROM Player p WHERE p.name = :name", Player.class)
                 .setParameter("name", name)
                 .uniqueResult();
     }
