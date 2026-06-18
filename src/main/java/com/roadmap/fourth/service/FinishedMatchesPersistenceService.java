de2ff70288fc9d4b4b7246f1e9667586215eea5d
Sweit2326 <filamilx@gmail.com>
2026-06-18 20:19:07 +0300

    full refactor

--- src/main/java/com/roadmap/fourth/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/roadmap/fourth/service/FinishedMatchesPersistenceService.java
@@ -1,43 +1,90 @@
 package com.roadmap.fourth.service;
 
-import com.roadmap.fourth.dao.InMemoryMatchDao;
+import com.roadmap.fourth.dao.MatchDao;
+import com.roadmap.fourth.dto.MatchScoreDTO;
+import com.roadmap.fourth.dto.PageDTO;
+import com.roadmap.fourth.exception.INTERNAL_SERVER_ERROR;
 import com.roadmap.fourth.model.*;
-import java.util.HashMap;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import java.util.List;
 
+import static com.roadmap.fourth.util.HibernateUtil.getSessionFactory;
+
 public class FinishedMatchesPersistenceService {
-    private final static InMemoryMatchDao MATCH_DAO = new InMemoryMatchDao();
+    private final static MatchDao MATCH_DAO = new MatchDao();
 
-    public Page getFinishedMatchesByPage(int pageNum) {
-        HashMap<Integer, Match> pageMatches = new HashMap<>();
+    public PageDTO getFinishedMatchesByPage(int pageNum) {
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
 
-        int pageNumX = (pageNum - 1) * 5 + 1;
-        for (int pos = 1; pos <= 5; pos++) {
-            int matchId = pageNumX + pos - 1;
-            pageMatches.put(pos, MATCH_DAO.getMatchById(matchId));
+        try {
+            int totalMatches = MATCH_DAO.getMatchesCount();
+            int totalPages = (totalMatches + 4) / 5;
+            int reqPageNum = Math.min(totalPages, pageNum);
+            int offset = (reqPageNum > 1) ? 5 * (reqPageNum - 1) : 0;
+            List<Match> matches = MATCH_DAO.getMatchesOnPage(offset);
+
+            tx.commit();
+            return new PageModel(mapMatchesToMatrix(matches), reqPageNum, totalPages).buildPageDTO();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            } else {
+                throw new INTERNAL_SERVER_ERROR("Database error happened, try again later");
+            }
         }
-
-        if (pageMatches.get(5) != null) {
-            return new Page(pageMatches, pageNum, true);
-        } else return new Page(pageMatches, pageNum, false);
+        return null;
     }
-    public Page getFinishedMatchesByPlayerName(int pageNum, String name) {
-        List<Match> matches = MATCH_DAO.getMatchesByPlayerName(name);
-        int fromIndex = (pageNum - 1) * 5;
-        int toIndex = Math.min(fromIndex+5, matches.size());
-        List<Match> subMatches = matches.subList(fromIndex, toIndex);
-        HashMap<Integer, Match> pageMatches = new HashMap<>();
 
-        for (int matchNum = 0; matchNum < subMatches.size(); matchNum++) {
-            pageMatches.put(matchNum, subMatches.get(matchNum));
+    public PageDTO getFinishedMatchesByPlayerName(int pageNum, String name) {
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        try {
+            int totalMatches = MATCH_DAO.getMatchesCountByPlayerName(name);
+            int totalPages = (totalMatches + 4) / 5;
+            int reqPageNum = (pageNum == 1) ? 1 : Math.min(totalPages, pageNum);
+            int offset = (reqPageNum > 1) ? 5 * (reqPageNum - 1) : 0;
+            List<Match> matches = MATCH_DAO.getMatchesByPlayerName(offset, name);
+
+            tx.commit();
+            return new PageModel(mapMatchesToMatrix(matches), reqPageNum, totalPages).buildPageDTO();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            } else {
+                throw new INTERNAL_SERVER_ERROR("Database error happened, try again later");
+            }
         }
-
-        return new Page(pageMatches, pageNum, toIndex != matches.size());
+        return null;
     }
-    public void postFinishedMatch(MatchScore matchScore) {
-        Player stPlayer = matchScore.getPlayer(0);
-        Player ndPlayer = matchScore.getPlayer(1);
-        Player winner = matchScore.getWinner();
-        MATCH_DAO.postMatch(stPlayer, ndPlayer, winner);
+
+    public void postFinishedMatch(MatchScoreDTO matchDTO) {
+        Session session = getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        try {
+            MATCH_DAO.postMatch(matchDTO);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null && tx.isActive()) {
+                tx.rollback();
+            } else {
+                throw new INTERNAL_SERVER_ERROR("Database error happened, try again later");
+            }
+        }
+    }
+
+    private String[][] mapMatchesToMatrix(List<Match> matches) {
+        int count = 0;
+        String[][] matrixMatches = new String[5][3];
+        for (Match m : matches) {
+            matrixMatches[count][0] = m.getPlayer1().getName();
+            matrixMatches[count][1] = m.getPlayer2().getName();
+            matrixMatches[count][2] = m.getWinner().getName();
+            count++;
+        }
+        return matrixMatches;
     }
 }
